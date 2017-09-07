5756f79394e310354e0a8a4c94dcd1d851422461
Eduardo Lopes <edulopestj@gmail.com>
2017-09-07 17:43:10 -0300

    add middleware cors in the application

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,11 @@
 const restify = require('restify')
 const routes = require('../http/routes')
+const cors = require('./cors')
+
 const server = restify.createServer()
+
+server.pre(cors.preflight)
+server.use(cors.actual)
 
 routes(server)
 
