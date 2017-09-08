95fd4dc0edaee9d2e26caa4fae01ba50f02abf19
Eduardo Lopes <edulopestj@gmail.com>
2017-09-08 18:39:41 -0300

    add endpoints to api rest

--- src/http/routes.js
+++ src/http/routes.js
@@ -4,6 +4,27 @@ const routes = (server) => {
     res.send('Enjoy the silence...')
     next()
   })
+
+  server.get('category', (req, res, next) => {
+    res.send([1, 'asas'])
+    next()
+  })
+
+  server.post('category', (req, res, next) => {
+    const { name } = req.params
+    res.send(name)
+    next()
+  })
+
+//   server.put('category', (req, res, next) => {
+//     res.send('category')
+//     next()
+//   })
+
+//   server.delete('category', (req, res, next) => {
+//     res.send('category')
+//     next()
+//   })
 }
 
 module.exports = routes
