526400adb3aa670ca02b3b3c8f14eb96a9b1d064
Eduardo Lopes <edulopestj@gmail.com>
2017-09-10 17:38:37 -0300

    leaving the block of code more readable

--- src/http/routes.js
+++ src/http/routes.js
@@ -2,13 +2,21 @@
 const db = require('../service/mysql')
 
 const routes = (server) => {
-  server.get('category', (req, res, next) => {
-    db.categories().all().then(categories => {
-      res.send(categories)
-    }).catch(error => {
-      res.send(error)
-    })
-    next()
+  server.get('category', async (req, res, next) => {
+    try {
+        res.send( await db.categories().all() )
+        next()
+    } catch {
+        res.send( error )
+        next()
+    }
+    
+    // db.categories().all().then(categories => {
+    //   res.send(categories)
+    // }).catch(error => {
+    //   res.send(error)
+    // })
+    // next()
   })
 
   server.post('category', (req, res, next) => {
