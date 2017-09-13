a8f7b9d38f7ad6fde3a9cba1e1cbca3e2ca416b0
Eduardo Lopes <edulopestj@gmail.com>
2017-09-13 12:03:44 -0300

    add dotenv

--- src/service/mysql/index.js
+++ src/service/mysql/index.js
@@ -2,11 +2,11 @@
 const mysqlServer = require('mysql')
 
 const connection = mysqlServer.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  port: '8889',
-  database: 'restful-ws'
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  port: process.env.MYSQL_PORT,
+  database: process.env.MYSQL_DATABASE
 })
 
 const errorHandler = (error, msg, rejectFunction) => {
